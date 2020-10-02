64aee8cf647b378b1ea7db2228aa08b8af3a4bbd
shouyamakichi <houyama5610@gmail.com>
2020-10-02 16:12:24 +0900

    ヘッダーアイコン実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,24 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+window.addEventListener('load', function(){
+  const pullDownButton = document.getElementById("menuButton")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener("mouseover", function(){
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+})
