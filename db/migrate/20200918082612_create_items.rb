447ba91f38ed7dda7950ee2c9cc6af47254beebb
shouyamakichi <houyama5610@gmail.com>
2020-10-12 21:53:02 +0900

    first commit

--- db/migrate/20200918082612_create_items.rb
+++ db/migrate/20200918082612_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name,                     null:false
       t.integer :price,                   null:false
       t.text :acount,                     null:false
-      t.integer :user_id                  null: false, foreign_key: true
+      t.integer :user_id,                  null: false, foreign_key: true
       t.timestamps
     end
   end
