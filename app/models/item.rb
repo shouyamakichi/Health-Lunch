db6b6702c134452bad53b80c3c5ff40516d71de2
shouyamakichi <houyama5610@gmail.com>
2020-10-13 13:39:38 +0900

    アソシエーション削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  belongs_to :user
   has_one_attached :image
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :items
   has_many :proteins
   has_many :softs
   has_many :calos
