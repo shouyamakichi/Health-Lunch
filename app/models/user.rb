a2c8629055f53ba7f97c15f114f41a4531d61711
shouyamakichi <houyama5610@gmail.com>
2020-10-13 14:45:53 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,17 +23,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-    validates :last_name
-    validates :first_name
-  end
+  # with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+  #   validates :last_name
+  #   validates :first_name
+  # end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
+  # with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+  #   validates :last_name_kana
+  #   validates :first_name_kana
+  # end
 
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
-  validates :email, uniqueness: { case_sensitive: true }
+  # validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  # validates :email, uniqueness: { case_sensitive: true }
 
 end
